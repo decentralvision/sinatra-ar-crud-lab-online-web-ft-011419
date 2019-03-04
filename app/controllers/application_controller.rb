b1b4ed80e8f5a37f021db914b7228860d0cb41cc
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-03-04 10:12:16 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,27 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles/new' do
+    erb new
+  end
+
+  get '/articles' do
+    @all = Article.all
+    erb :index
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params["id"])
+    erb :show
+  end
+
+  post '/articles' do
+    Article.create
+    erb :index
+  end
+
+  get '/articles/:id/edit' do
+    erb :edit
+  end
 end
